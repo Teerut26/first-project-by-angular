bb9c10ae66ea76a2048c13333ba6e443602fe66a
Teerut26 <teerutsr@gmail.com>
2022-02-01 18:20:32 +0700

    🔥first project by andgular.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'my-first-app';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    console.log(555);
+  }
+
+  decrease(): void {
+    this.count -= 1;
+  }
+
+  increase(): void {
+    this.count += 1;
+  }
+  
+  count = 0;
 }
